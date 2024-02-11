d02223b5def271a1dcb0228cdd2d5c23f2ec1e61
Arvin Duh <150882116+shogun-olives@users.noreply.github.com>
2024-02-11 03:36:17 -0800

    Basic metadata search

--- file_helper/format_txt.py
+++ file_helper/format_txt.py
@@ -1,2 +1,67 @@
-def rename_move(dir_name, target_dir):
+import os
+from datetime import datetime
+
+
+def find_index(search: list[str], arr: list[str]) -> int | None:
+    for word in search:
+        try:
+            index = arr.index(word)
+            return index
+        except ValueError:
+            pass
+    return None
+
+
+def extract_from_title(fn: str) -> (str, str, str):
+    fn = os.path.basename(fn)
+    words = fn.removesuffix('.txt').split(' ')
+
+    corp_suffixes = ['Inc', 'Corp']
+
+    call_index = find_index(corp_suffixes, words) + 1
+    date_index = words.index('Call') + 1
+
+    company_name = ' '.join(words[:call_index])
+    presentation_type = ' '.join(words[call_index:date_index])
+    presentation_id = words[-1]
+
+    return presentation_id, company_name, presentation_type
+
+
+def get_symbol(line: str) -> str | None:
+    words = line.split(' ')
+    for word in words:
+        if '(' in word:
+            return word.removeprefix('(')
+    return None
+
+
+def get_data(fn) -> dict[str: str]:
+    presentation_id, company_name, presentation_type = extract_from_title(fn)
+
+    with open(fn, 'r', encoding='utf-8') as file:
+        line = file.readline().removeprefix('FINAL TRANSCRIPT').strip()
+        date = datetime.strptime(line, '%Y-%m-%d').date()
+
+        line = file.readline()
+        symbol = get_symbol(line)
+
+    data = {
+        'ID': presentation_id,
+        'Company': company_name,
+        'Symbol': symbol,
+        'Date': date,
+        'Type': presentation_type,
+        'File': fn
+    }
+
+    return data
+
+
+def main() -> None:
+    get_data('../raw_files/transcripts/txt/Alphabet Inc Conf Presentation Call 2015304 FS000000002197545639.txt', '')
     pass
+
+
+if __name__ == '__main__':
+    main()
