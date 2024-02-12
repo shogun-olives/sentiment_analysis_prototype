93eb2f4d03a7bd8e2e8976495a1508d5ba13da5d
Arvin Duh <150882116+shogun-olives@users.noreply.github.com>
2024-02-11 16:46:17 -0800

    Updating test structure

---
+++ test_file_helper/__init__.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+proj_path = os.getcwd()
+src_path = os.path.join(proj_path, "src")
+sys.path.append(src_path)
--- test_file_helper/test_extract_metadata.py
+++ test_file_helper/test_extract_metadata.py
@@ -5,7 +5,7 @@ from file_helper import extract_metadata as em
 class TestMyModule(unittest.TestCase):
     def test_get_symbol(self):
         result = em.get_symbol('Alphabet Inc (GOOGL US Equity)')
-        self.assertEqual(result, "GOOGL")
+        self.assertEqual(result, "GOOG")
 
 
 if __name__ == '__main__':
