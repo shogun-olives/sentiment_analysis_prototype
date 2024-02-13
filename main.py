b3a57db4f020c29ddf0aad95a84fa6f2f0f908b2
Arvin Duh <150882116+shogun-olives@users.noreply.github.com>
2024-02-12 16:38:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ import file_helper as fh
 import pandas as pd
 
 
-def format_data(pdf_loc, txt_loc, format_txt_loc, db_name) -> None:
+def format_data(pdf_loc, txt_loc, format_txt_loc):
     fh.all_pdf_to_txt(pdf_loc, txt_loc, overwrite=False)
     meta_df = fh.get_metadata_df(txt_loc)
 
     txt_df = fh.format_all_txt(txt_loc, format_txt_loc)
 
     df = pd.merge(meta_df, txt_df, on='ID', how='outer')
-    fh.df_to_db(df, db_name, 'transcripts')
-
-
-def get_data(db_name) -> pd.DataFrame:
-    df = fh.db_to_df(db_name, 'transcripts')
     return df
 
 
@@ -23,9 +18,9 @@ def main() -> None:
     format_txt_loc = './files/transcripts'
     db_name = './files/sentiment_analysis_data.db'
 
-    format_data(pdf_loc, txt_loc, format_txt_loc, db_name)
-    # df = get_data(db_name)
-    # print(df)
+    # df = format_data(pdf_loc, txt_loc, format_txt_loc)
+    # fh.df_to_db(df, db_name, 'transcripts')
+    # df = fh.db_to_df(db_name, 'transcripts')
     pass
 
 
