f831d3c1f114a59e0ece6d9fef395541c4bde141
Arvin Duh <150882116+shogun-olives@users.noreply.github.com>
2024-02-12 16:32:31 -0800

    Renamed database

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ import file_helper as fh
 import pandas as pd
 
 
-def format_data() -> None:
-    pdf_loc = './raw/transcripts/pdf'
-    txt_loc = './raw/transcripts/txt'
-
+def format_data(pdf_loc, txt_loc, format_txt_loc, db_name) -> None:
     fh.all_pdf_to_txt(pdf_loc, txt_loc, overwrite=False)
     meta_df = fh.get_metadata_df(txt_loc)
 
-    format_txt_loc = './files/transcripts'
-    db_name = './files/transcript_metadata.db'
-
     txt_df = fh.format_all_txt(txt_loc, format_txt_loc)
+
     df = pd.merge(meta_df, txt_df, on='ID', how='outer')
     fh.df_to_db(df, db_name, 'transcripts')
 
 
-def get_data() -> pd.DataFrame:
-    db_name = './files/transcript_metadata.db'
+def get_data(db_name) -> pd.DataFrame:
     df = fh.db_to_df(db_name, 'transcripts')
     return df
 
 
 def main() -> None:
-    # format_data()
-    # print(get_data())
+    pdf_loc = './raw/transcripts/pdf'
+    txt_loc = './raw/transcripts/txt'
+    format_txt_loc = './files/transcripts'
+    db_name = './files/sentiment_analysis_data.db'
+
+    format_data(pdf_loc, txt_loc, format_txt_loc, db_name)
+    # df = get_data(db_name)
+    # print(df)
     pass
 
 
